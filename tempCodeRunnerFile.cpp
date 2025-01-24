7b7d6a70079414905e1928bbfd9021f28a20cf15
Krish Pal <dev974437@gmail.com>
2025-01-24 16:39:31 +0530

    Only first assignment

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,24 +1,10 @@
 #include<iostream>
 using namespace std;
-#define null 0;
-#define capacity_invalid 1;
- class Array{
-    private:
-    int capacity;
-    int lastIndex;
-    int *ptr;
-    public:
-    Array(int);
-};
-Array::Array(int size){
-    ptr=nullptr;
-    if(size<1)
-      throw capacity_invalid;
-
-
-    ptr=new int[size];
-
-}
 int main(){
-    Array a1(4);
+    int a;
+    for(int i=1;i<=10;i++){
+       cout<<2*i<<endl;
+    }
+  
+
 }
